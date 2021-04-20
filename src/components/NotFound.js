12e1446da592a8c07dcd1aa11670105e835aac66
rocketgene <78985165+rocketgene@users.noreply.github.com>
2021-04-21 00:12:35 +0100

    Add files via upload

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+
+function NotFound() {
+    return (
+        <div className="not-found">
+            <h3>No Results Found</h3>
+            <p>You search did not return any results. Please try again.</p>
+        </div>
+    )
+}
+
+export default NotFound;
