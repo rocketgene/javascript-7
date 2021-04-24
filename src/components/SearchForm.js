0a72475273ba80d08f20762b3e73e50f420a8795
rocketgene <78985165+rocketgene@users.noreply.github.com>
2021-04-24 20:27:54 +0100

    cleared unused assets

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { useHistory, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 class SearchForm extends Component {
     
     handleSubmit = (e) => {
